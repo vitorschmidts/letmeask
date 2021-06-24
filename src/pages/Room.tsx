68d4249f35bdec3bc25b4815039ed2021a7a9b8f
vitor <schmidt_vitor@hotmail.com>
2021-06-24 12:51:50 -0300

    mudanças de estilo

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,36 @@
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/button';
+
+import '../styles/room.scss'
+
 export function Room() {
 	return (
-		<h1>Salas</h1>
+		<div className="page-room">
+			<header>
+				<div className="content">
+					<img src={logoImg} alt="logo" />
+					<div>codigo</div>
+				</div>
+			</header>
+
+			<main className="content">
+				<div className="room-title">
+					<h1>Sala React</h1>
+					<span>4 perguntas</span>
+				</div>
+
+				<form action="">
+					<textarea
+						placeholder="O que você quer perguntar?"
+					/>
+
+				<div className="form-footer">
+					<span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+					<Button type="submit">Enviar pergunta</Button>
+				</div>
+				</form>
+			</main>
+		</div>
 	)
 }
