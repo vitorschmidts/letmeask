58223eba7d38913998976db7fe6af23cf87623d3
vitor <schmidt_vitor@hotmail.com>
2021-06-24 15:53:12 -0300

    estilo room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,12 +1,13 @@
+import '../styles/room.scss';
+
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/button';
 
-import '../styles/room.scss'
 
 export function Room() {
 	return (
-		<div className="page-room">
+		<div id="page-room">
 			<header>
 				<div className="content">
 					<img src={logoImg} alt="logo" />
@@ -14,7 +15,7 @@ export function Room() {
 				</div>
 			</header>
 
-			<main className="content">
+			<main>
 				<div className="room-title">
 					<h1>Sala React</h1>
 					<span>4 perguntas</span>
